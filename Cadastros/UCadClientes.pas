a4c36c034240768faa39cb391145013caa924a95
janiltojack@yahoo.com.br <jackanilto>
2021-01-31 01:12:39 -0300

    Cadstro Clientes Funcionando

--- Cadastros/UCadClientes.pas
+++ Cadastros/UCadClientes.pas
@@ -8,47 +8,47 @@ uses
   Vcl.ComCtrls, Vcl.StdCtrls, Vcl.Buttons, Vcl.ExtDlgs, Vcl.ExtCtrls, Data.DB,
   Vcl.Grids, Vcl.DBGrids, Vcl.Mask, Vcl.AppEvnts, JvExMask, JvToolEdit,
   JvMaskEdit, JvCheckedMaskEdit, JvDatePickerEdit, JvAVICapture, Vcl.Imaging.jpeg, frxDesgn,
-  frxClass, frxExportBaseDialog, frxExportPDF, Vcl.DBCtrls;
+  frxClass, frxExportBaseDialog, frxExportPDF, Vcl.DBCtrls, Vcl.Menus;
 
 type
   TFrmCadClientes = class(TForm)
-    PageControl1: TPageControl;
-    TabSheet1: TTabSheet;
-    btnNovo: TBitBtn;
-    BtnSalvar: TBitBtn;
-    btnEditar: TBitBtn;
-    btnDeletar: TBitBtn;
     Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Estado: TLabel;
+    Label8: TLabel;
+    LabelCOMPLENENTO: TLabel;
+    Label7: TLabel;
+    Label9: TLabel;
+    Label10: TLabel;
+    Email: TLabel;
+    Label11: TLabel;
     EditNOME: TEdit;
     grid: TDBGrid;
-    EditID: TEdit;
-    Label2: TLabel;
     edtBuscar: TEdit;
     EditENDERECO: TEdit;
     EditNUMERO: TEdit;
     EditBAIRRO: TEdit;
     EditCIDADE: TEdit;
-    Label3: TLabel;
-    Label4: TLabel;
-    Label5: TLabel;
-    Label6: TLabel;
     EditESTADO: TEdit;
     EditCOMPLEMENTO: TEdit;
     EditTELPESSOAL: TEdit;
     EditCONTATO1: TEdit;
     EditCONTATO2: TEdit;
-    Estado: TLabel;
-    Label8: TLabel;
     MaskCEP: TMaskEdit;
-    LabelCOMPLENENTO: TLabel;
-    Label7: TLabel;
-    Label9: TLabel;
-    Label10: TLabel;
     EditEMAIL: TEdit;
-    Email: TLabel;
-    btnCartas: TSpeedButton;
     EditCNPJCPF: TEdit;
-    Label11: TLabel;
+    Panel1: TPanel;
+    btnNovo: TBitBtn;
+    BtnSalvar: TBitBtn;
+    btnEditar: TBitBtn;
+    btnDeletar: TBitBtn;
+    btnSAIR: TSpeedButton;
+    EditID: TEdit;
+    Label12: TLabel;
     procedure btnNovoClick(Sender: TObject);
     procedure BtnSalvarClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
@@ -58,6 +58,12 @@ type
     procedure edtBuscarChange(Sender: TObject);
     procedure gridDrawColumnCell(Sender: TObject; const Rect: TRect;
       DataCol: Integer; Column: TColumn; State: TGridDrawState);
+    procedure btnFecharClick(Sender: TObject);
+    procedure btnSAIRClick(Sender: TObject);
+    procedure Novo1Click(Sender: TObject);
+    procedure Salva1Click(Sender: TObject);
+    procedure Editar1Click(Sender: TObject);
+    procedure Deletar1Click(Sender: TObject);
 
 //    procedure btnCANCELARClick(Sender: TObject);
 
@@ -92,19 +98,19 @@ uses UDM;
 
 procedure TFrmCadClientes.associarCampos;
   begin
-     DM.TBL_CLIENTES.FieldByName('NOME').asstring := EditNOME.Text;
-     DM.TBL_CLIENTES.FieldByName('ENDERECO').asstring := EditENDERECO.Text;
-     DM.TBL_CLIENTES.FieldByName('NUMERO').asstring := EditNUMERO.Text;
-     DM.TBL_CLIENTES.FieldByName('BAIRRO').asstring := EditBAIRRO.Text;
-     DM.TBL_CLIENTES.FieldByName('CIDADE').asstring := EditCIDADE.Text;
-     DM.TBL_CLIENTES.FieldByName('ESTADO').asstring := EditESTADO.Text;
-     DM.TBL_CLIENTES.FieldByName('CEP').asstring := MaskCEP.Text;
-     DM.TBL_CLIENTES.FieldByName('COMPLEMENTO').asstring := EditCOMPLEMENTO.Text;
-     DM.TBL_CLIENTES.FieldByName('TELPESSOAL')  .Value   := EditTELPESSOAL.Text;
-     DM.TBL_CLIENTES.FieldByName('CONTATO1')  .Value   := EditCONTATO1.Text;
-     DM.TBL_CLIENTES.FieldByName('CONTATO2')  .Value   := EditCONTATO2.Text;
-     DM.TBL_CLIENTES.FieldByName('EMAIL')  .asstring   := EditEMAIL                .Text;
-     DM.TBL_CLIENTES.FieldByName('CNPJCPF')  .Value   := EditCNPJCPF              .Text;
+     DM.TBL_CLIENTES.FieldByName('NOME')        .asstring := EditNOME.Text;
+     DM.TBL_CLIENTES.FieldByName('ENDERECO')    .asstring := EditENDERECO.Text;
+     DM.TBL_CLIENTES.FieldByName('NUMERO')      .asstring := EditNUMERO.Text;
+     DM.TBL_CLIENTES.FieldByName('BAIRRO')      .asstring := EditBAIRRO.Text;
+     DM.TBL_CLIENTES.FieldByName('CIDADE')      .asstring := EditCIDADE.Text;
+     DM.TBL_CLIENTES.FieldByName('ESTADO')      .asstring := EditESTADO.Text;
+     DM.TBL_CLIENTES.FieldByName('CEP')         .asstring := MaskCEP.Text;
+     DM.TBL_CLIENTES.FieldByName('COMPLEMENTO') .asstring := EditCOMPLEMENTO.Text;
+     DM.TBL_CLIENTES.FieldByName('TELPESSOAL')  .Value    := EditTELPESSOAL.Text;
+     DM.TBL_CLIENTES.FieldByName('CONTATO1')    .Value    := EditCONTATO1.Text;
+     DM.TBL_CLIENTES.FieldByName('CONTATO2')    .Value    := EditCONTATO2.Text;
+     DM.TBL_CLIENTES.FieldByName('EMAIL')       .asstring := EditEMAIL.Text;
+     DM.TBL_CLIENTES.FieldByName('CNPJCPF')     .AsString := EditCNPJCPF.Text;
 
   end;
 
@@ -131,7 +137,6 @@ if Messagedlg('Deseja excluir o registro?', mtConfirmation, [mbYes, mbNo], 0) = 
     btnSalvar.Enabled := false;
     btnEditar.Enabled := false;
     btnDeletar.Enabled := false;
-    btnCartas.Enabled := false;
     btnNovo.Enabled := true;
   end;
 end;
@@ -144,43 +149,23 @@ begin
         DM.TBL_CLIENTES.Edit;
         DM.QueryClientes.Close;
         DM.QueryClientes.SQL.Clear;
+        DM.QueryClientes.SQL.Add('update TBL_CLIENTES set '+
+       ' NOME              =  :NOME                 , '+
+       ' ENDERECO          =  :ENDERECO             , '+
+       ' NUMERO            =  :NUMERO               , '+
+       ' BAIRRO            =  :BAIRRO               , '+
+       ' CIDADE            =  :CIDADE               , '+
+       ' ESTADO            =  :ESTADO               , '+
+       ' CEP               =  :CEP                  , '+
+       ' COMPLEMENTO       =  :COMPLEMENTO          , '+
+       ' TELPESSOAL        =  :TELPESSOAL           , '+
+       ' CONTATO1          =  :CONTATO1             , '+
+       ' CONTATO2          =  :CONTATO2             , '+
+       ' EMAIL             =  :EMAIL                , '+
+       ' CNPJCPF           =  :CNPJCPF                '+
+       ' where id  = :id');
 
-        if alterou = false then
-        begin
-          DM.QueryClientes.SQL.Add('update TBL_CLIENTES set   NOME = :NOME, '+
-         ' ENDERECO          =  :ENDERECO             , '+
-         ' NUMERO            =  :NUMERO               , '+
-         ' BAIRRO            =  :BAIRRO               , '+
-         ' CIDADE            =  :CIDADE               , '+
-         ' ESTADO            =  :ESTADO               , '+
-         ' CEP               =  :CEP                  , '+
-         ' COMPLEMENTO       =  :COMPLEMENTO          , '+
-         ' TELPESSOAL        =  :TELPESSOAL           , '+
-         ' CONTATO1          =  :CONTATO1             , '+
-         ' CONTATO2          =  :CONTATO2             , '+
-         ' EMAIL             =  :EMAIL                , '+
-         ' where id  = :id');
-
-          end
-          else
-          begin
-          DM.QueryClientes.SQL.Add('update TBL_CLIENTES set   NOME = :NOME, '+
-         ' ENDERECO          =  :ENDERECO             , '+
-         ' NUMERO            =  :NUMERO               , '+
-         ' BAIRRO            =  :BAIRRO               , '+
-         ' CIDADE            =  :CIDADE               , '+
-         ' ESTADO            =  :ESTADO               , '+
-         ' CEP               =  :CEP                  , '+
-         ' COMPLEMENTO       =  :COMPLEMENTO          , '+
-         ' TELPESSOAL        =  :TELPESSOAL           , '+
-         ' CONTATO1          =  :CONTATO1             , '+
-         ' CONTATO2          =  :CONTATO2             , '+
-         ' EMAIL             =  :EMAIL                , '+
-         ' where id  = :id');
-        end;
-
-
-        DM.QueryClientes.ParamByName('NOME').AsString := editNome.Text;
+    DM.QueryClientes.ParamByName('NOME').AsString := editNome.Text;
         DM.QueryClientes.ParamByName('ENDERECO').AsString := EditENDERECO.Text;
         DM.QueryClientes.ParamByName('NUMERO').AsString := editNUMERO.Text;
         DM.QueryClientes.ParamByName('BAIRRO').AsString := editBAIRRO.Text;
@@ -194,7 +179,6 @@ begin
         DM.QueryClientes.ParamByName('EMAIL').Value := editEMAIL.Text;
         DM.QueryClientes.ParamByName('CNPJCPF').Value := EditCNPJCPF.Text;
         DM.QueryClientes.ParamByName('id').Value := editID.Text;
-
         DM.QueryClientes.ExecSql;
 
         MessageDlg('Editado com Sucesso!!', mtInformation, mbOKCancel, 0);
@@ -204,7 +188,6 @@ begin
         btnSalvar.Enabled := false;
         btnEditar.Enabled := false;
         btnDeletar.Enabled := false;
-        btnCartas.Enabled := false;
         btnNovo.Enabled := true;
         grid.Enabled := true;
     end
@@ -242,7 +225,6 @@ begin
     btnNovo.Enabled := true;
     btnEditar.Enabled := false;
     btnDeletar.Enabled := false;
-    btnCartas.Enabled := false;
     grid.Enabled := true;
     end
     else
@@ -268,11 +250,98 @@ begin
   DM.QueryClientes.Open();
 end;
 
+procedure TFrmCadClientes.Deletar1Click(Sender: TObject);
+begin
+if Messagedlg('Deseja excluir o registro?', mtConfirmation, [mbYes, mbNo], 0) = mrYes then
+  begin
+   associarCampos;
+    DM.QueryClientes.Close;
+    DM.QueryClientes.SQL.Clear;
+    DM.QueryClientes.SQL.Add('delete from TBL_CLIENTES where ID = :ID');
+
+    DM.QueryClientes.ParamByName('ID').Value := editID.Text;
+    DM.QueryClientes.ExecSql;
+    buscarTudo;
+    MessageDlg('Excluido com Sucesso!!', mtInformation, mbOKCancel, 0);
+    // destativa a table (solução para atualiza grid em tempo de execução)
+    //reativa a table
+    DM.TBL_CLIENTES.Active := false;
+    DM.TBL_CLIENTES.Active := true;
+
+    EditNOME.Enabled := false;
+    btnSalvar.Enabled := false;
+    btnEditar.Enabled := false;
+    btnDeletar.Enabled := false;
+    btnNovo.Enabled := true;
+  end;
+end;
+
+procedure TFrmCadClientes.btnFecharClick(Sender: TObject);
+begin
+FrmCadClientes.Close;
+end;
+
 procedure TFrmCadClientes.desabilitarCampos;
 begin
 limparcampos();
 EditNOME.Enabled := false;
 
+end;
+
+procedure TFrmCadClientes.Editar1Click(Sender: TObject);
+begin
+  if (editNOME.Text <> '') then
+    begin
+        associarCampos;
+        DM.TBL_CLIENTES.Edit;
+        DM.QueryClientes.Close;
+        DM.QueryClientes.SQL.Clear;
+        DM.QueryClientes.SQL.Add('update TBL_CLIENTES set '+
+       ' NOME              =  :NOME                 , '+
+       ' ENDERECO          =  :ENDERECO             , '+
+       ' NUMERO            =  :NUMERO               , '+
+       ' BAIRRO            =  :BAIRRO               , '+
+       ' CIDADE            =  :CIDADE               , '+
+       ' ESTADO            =  :ESTADO               , '+
+       ' CEP               =  :CEP                  , '+
+       ' COMPLEMENTO       =  :COMPLEMENTO          , '+
+       ' TELPESSOAL        =  :TELPESSOAL           , '+
+       ' CONTATO1          =  :CONTATO1             , '+
+       ' CONTATO2          =  :CONTATO2             , '+
+       ' EMAIL             =  :EMAIL                , '+
+       ' CNPJCPF           =  :CNPJCPF                '+
+       ' where id  = :id');
+
+    DM.QueryClientes.ParamByName('NOME').AsString := editNome.Text;
+        DM.QueryClientes.ParamByName('ENDERECO').AsString := EditENDERECO.Text;
+        DM.QueryClientes.ParamByName('NUMERO').AsString := editNUMERO.Text;
+        DM.QueryClientes.ParamByName('BAIRRO').AsString := editBAIRRO.Text;
+        DM.QueryClientes.ParamByName('CIDADE').AsString := editCIDADE.Text;
+        DM.QueryClientes.ParamByName('ESTADO').AsString := editESTADO.Text;
+        DM.QueryClientes.ParamByName('CEP').Value := MaskCEP.Text;
+        DM.QueryClientes.ParamByName('COMPLEMENTO').Value := editCOMPLEMENTO.Text;
+        DM.QueryClientes.ParamByName('TELPESSOAL').Value := editTELPESSOAL.Text;
+        DM.QueryClientes.ParamByName('CONTATO1').Value := editCONTATO1.Text;
+        DM.QueryClientes.ParamByName('CONTATO2').Value := editCONTATO2.Text;
+        DM.QueryClientes.ParamByName('EMAIL').Value := editEMAIL.Text;
+        DM.QueryClientes.ParamByName('CNPJCPF').Value := EditCNPJCPF.Text;
+        DM.QueryClientes.ParamByName('id').Value := editID.Text;
+        DM.QueryClientes.ExecSql;
+
+        MessageDlg('Editado com Sucesso!!', mtInformation, mbOKCancel, 0);
+        buscarTudo;
+        desabilitarCampos;
+        limparCampos;
+        btnSalvar.Enabled := false;
+        btnEditar.Enabled := false;
+        btnDeletar.Enabled := false;
+        btnNovo.Enabled := true;
+        grid.Enabled := true;
+    end
+    else
+    begin
+    MessageDlg('Preencha os Campos', mtInformation, mbOKCancel, 0);
+    end;
 end;
 
 procedure TFrmCadClientes.edtBuscarChange(Sender: TObject);
@@ -291,7 +360,6 @@ limparCampos;
 btnSalvar.Enabled := false;
 btnEditar.Enabled := false;
 btnDeletar.Enabled := false;
-btnCartas.Enabled := false;
 end;
 
 procedure TFrmCadClientes.gridCellClick(Column: TColumn);
@@ -300,7 +368,6 @@ begin
   DM.QueryClientes.Edit;
   btnEditar.Enabled := true;
   btnDeletar.Enabled := true;
-  btnCartas.Enabled := true;
   habilitarCampos;
 
   if DM.QueryClientes.FieldByName('NOME').Value <> null then
@@ -397,6 +464,47 @@ begin
   EditCONTATO2     .Text  := '';
   EditEMAIL        .Text  := '';
   EditCNPJCPF      .Text  := '';
+end;
+
+procedure TFrmCadClientes.Novo1Click(Sender: TObject);
+begin
+  //apos editar um dado esta desbilitando o Edit
+  grid.Enabled:=false;
+  EditNOME.Enabled := true; // Reabilita Edit
+  EditNOME.Text := '';
+  EditNOME.SetFocus;
+  DM.TBL_CLIENTES.Insert;
+  BtnSalvar.Enabled := true;
+  btnNovo.Enabled := true;
+  btnEditar.Enabled := true;
+  btnDeletar.Enabled := false;
+
+end;
+
+procedure TFrmCadClientes.Salva1Click(Sender: TObject);
+begin
+  if (EditNOME.Text <> '') then
+  begin
+    associarCampos;
+    DM.TBL_CLIENTES.Post;
+    MessageDlg('Salvo com Sucesso!!', mtInformation, mbOKCancel, 0);
+    buscarTudo;
+    desabilitarCampos;
+    btnSalvar.Enabled := false;
+    btnNovo.Enabled := true;
+    btnEditar.Enabled := false;
+    btnDeletar.Enabled := false;
+    grid.Enabled := true;
+    end
+    else
+    begin
+    MessageDlg('Preencha os Campos', mtInformation, mbOKCancel, 0);
+  end;
+end;
+
+procedure TFrmCadClientes.btnSAIRClick(Sender: TObject);
+begin
+FrmCadClientes.Close;
 end;
 
 end.
